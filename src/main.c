0d522d5edf64aed31ce827d604654f7929f03601
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-05-15 10:33:49 +0200

    feat: stop and cont

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "shell.h"
 #include "tokenizer.h"
+#include <stdlib.h>
 #include <signal.h>
-#include <math.h>
+#include <fcntl.h>
 #include <linux/limits.h>
+#include <math.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,9 +15,9 @@
 
 #define CMDCHARS 512
 
-void handle_SIGINT(int _signal) {
-    printf("\n\n\033[90m[To quit, type \033[97mexit\033[90m twice]\033[97m\n\n");
-}
+pid_t fg_proc = 0;
+pid_t old_fg_proc = 0;
+char *cmdbuf;
 
 void outputShell(char *cmdbuf) {
   char cwd[PATH_MAX];
@@ -88,69 +90,151 @@ void readCmd(char *cmdbuf) {
   }
 }
 
-int numPlaces (int n) {
-    if (n == 0) return 1;
-    return floor (log10 (abs (n))) + 1;
+int numPlaces(int n) {
+  if (n == 0)
+    return 1;
+  return floor(log10(abs(n))) + 1;
+}
+
+void handle_SIGINT(int _signal) {
+    if (fg_proc == 0) return;
+    kill(fg_proc, SIGINT);
+    printf("\n\033[90m[%d] killed\033[97m\n", fg_proc);
+}
+
+void handle_SIGTSTP(int _signal) {
+    if (fg_proc == 0) return;
+    kill(fg_proc, SIGTSTP);
+    old_fg_proc = fg_proc;
+    printf("\n\033[90m[%d] stopped\033[97m\n", fg_proc);
+}
+
+void handle_SIGCHLD(int _signal) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 int main() {
-  char *cmdbuf = calloc(CMDCHARS, sizeof(char));
+  cmdbuf = calloc(CMDCHARS, sizeof(char));
   struct winsize w;
-  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); // get winsize
   signal(SIGINT, handle_SIGINT);
+  signal(SIGTSTP, handle_SIGTSTP);
+  signal(SIGCHLD, handle_SIGCHLD);
 
   bool last_was_exit = false;
   while (true) {
     readCmd(cmdbuf);
     if (*cmdbuf == 0) {
-        printf("\n");
-        continue;
+      printf("\r\n");
+      continue;
     }
     token_t *tokens = tokenize(cmdbuf);
     char **command = tokens_to_command(tokens);
     bool is_continue = should_continue(tokens);
 
     if (strcmp(*command, "exit") == 0) {
-        if (!last_was_exit) {
-            printf("\n\033[90m[To confirm, type \033[97mexit\033[90m again]\033[97m\n");
-            last_was_exit = true;
-        } else {
-            printf("\n\033[90m[Bye]\033[97m\n");
-            exit(0);
-        }
-        free_command(command);
-        free_tokens(tokens);
-        continue;
+      if (!last_was_exit) {
+        printf("\n\033[90m[To confirm, type \033[97mexit\033[90m "
+               "again]\033[97m\n");
+        last_was_exit = true;
+      } else {
+        printf("\n\033[90m[Bye]\033[97m\n");
+        exit(0);
+      }
+      free_command(command);
+      free_tokens(tokens);
+      continue;
     }
 
     if (strcmp(*command, "cd") == 0) {
-        char *arg = *(command+1);
-        printf("\n");
-        if (arg == NULL) {
-            printf("\033[90mUsage: \033[97mcd [path]\033[97m\n");
-        } else {
-            if (chdir(arg) != 0) {
-                printf("\033[31mError\033[97m: No such file or directory\033[97m\n");
-            }
+      char *arg = *(command + 1);
+      printf("\n");
+      if (arg == NULL) {
+        printf("\033[90mUsage: \033[97mcd [path]\033[97m\n");
+      } else {
+        if (chdir(arg) != 0) {
+          printf("\033[31mError\033[97m: No such file or directory\033[97m\n");
         }
+      }
 
-        free_command(command);
-        free_tokens(tokens);
-        continue;
+      free_command(command);
+      free_tokens(tokens);
+      continue;
+    }
+
+    if (strcmp(*command, "stop") == 0) {
+      char *arg = *(command + 1);
+      printf("\n");
+      if (arg == NULL) {
+        printf("\033[90mUsage: \033[97mstop [pid]\033[97m\n");
+      } else {
+        int val = atoi(arg);
+        if (val == 0) {
+          printf("\033[31mError\033[97m: No such file or directory\033[97m\n");
+        } else {
+          kill((pid_t)val, SIGTSTP);
+        }
+      }
+
+      free_command(command);
+      free_tokens(tokens);
+      continue;
+    }
+
+    if (strcmp(*command, "cont") == 0) {
+      char *arg = *(command + 1);
+      printf("\n");
+      if (arg == NULL) {
+        printf("\033[90mUsage: \033[97mstop [pid]\033[97m\n");
+      } else {
+        int val = atoi(arg);
+        if (val == 0) {
+          printf("\033[31mError\033[97m: No such file or directory\033[97m\n");
+        } else {
+          kill((pid_t)val, SIGCONT);
+          if (val == old_fg_proc) {
+              old_fg_proc = 0;
+              fg_proc = val;
+              waitpid(val, NULL, 0);
+          }
+        }
+      }
+
+      free_command(command);
+      free_tokens(tokens);
+      continue;
+    }
+
+    if (strcmp(*command, "pwd") == 0) {
+      char cwd[PATH_MAX];
+      if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        cwd[0] = '?';
+        cwd[1] = 0;
+      }
+      printf("\n%s\n\n", cwd);
+
+      free_command(command);
+      free_tokens(tokens);
+      continue;
     }
 
     last_was_exit = false;
 
     pid_t pid = fork();
     if (pid == 0) {
+      pid_t my_id = getpid();
+      setpgid(0, my_id);
       execvp(command[0], command);
       perror("\033[31mError\033[97m");
       exit(1);
     } else if (pid > 0) {
-      printf("\r\033[%dC\033[37mPID: \033[33m%d\033[97m\n", w.ws_col - numPlaces(pid) - 6, pid);
+      printf("\r\033[%dC\033[37mPID: \033[33m%d\033[97m\n",
+             w.ws_col - numPlaces(pid) - 6, pid);
       if (!is_continue) {
+        fg_proc = pid;
         int status;
-        waitpid(pid, &status, 0);
+        waitpid(pid, &status, WUNTRACED);
+        fg_proc = 0;
       }
     } else {
       perror("fork");
